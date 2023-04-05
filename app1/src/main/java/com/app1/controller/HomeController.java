c7f746037deb80251cabc7bb1138eb1a4d1e8180
sandeep kumar <kmr.sandeep1997@gmail.com>
2023-04-05 12:25:36 +0530

    app1 circuit breaker changes done

--- app1/src/main/java/com/app1/controller/HomeController.java
+++ app1/src/main/java/com/app1/controller/HomeController.java
@@ -2,22 +2,19 @@ package com.app1.controller;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
-import io.github.resilience4j.decorators.Decorators;
-import io.vavr.control.Try;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import java.util.function.Supplier;
-import java.util.Arrays;
-import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
-import java.util.concurrent.TimeoutException;
-import org.springframework.web.client.RestClientException;
+
 
 
 @RestController
 public class HomeController {
 
+    public static final String CIRCUIT_BREAKER = "breaker";
     private RestTemplate restTemplate;
 
     private CircuitBreaker circuitBreaker;
@@ -34,37 +31,24 @@ public class HomeController {
         return "this is message from app1";
     }
 
+
     @GetMapping("/call-app2")
+    @io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker(name = CIRCUIT_BREAKER,fallbackMethod= "FallBackMessage")
     public String callApp2() {
-        // Define the URL of App2's endpoint
-        String app2Url = "http://localhost:8082/hello";
 
-        // Create a Supplier that makes a call to App2 using RestTemplate
-        Supplier<String> supplier = () -> restTemplate.getForObject(app2Url, String.class);
+        String fooResourceUrl
+                = "http://localhost:8082/hello";
+        ResponseEntity<String> response
+                = restTemplate.getForEntity(fooResourceUrl , String.class);
+        //Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
 
-        // Decorate the Supplier with a Circuit Breaker and a Fallback function
-        // The fallback function will be called when any of the specified exceptions are thrown
-        Supplier<String> decoratedSupplier = Decorators.ofSupplier(supplier)
-                .withCircuitBreaker(circuitBreaker)
-                .withFallback(Arrays.asList(TimeoutException.class,
-                                CallNotPermittedException.class,
-                                RestClientException.class),
-                        // Fallback function to handle specified exceptions
-                        throwable -> app2Fallback())
-                .decorate();
-
-        // Execute the decorated Supplier and store the result in a Try instance
-        Try<String> result = Try.ofSupplier(decoratedSupplier);
-
-        // If the Try instance contains a successful result, return it
-        // Otherwise, return the result of the fallback function
-        return result.getOrElseGet(throwable -> app2Fallback());
+        return "app1 called";
     }
+    public String FallBackMessage(Exception e){
 
-    // Fallback function to be called when the Circuit Breaker is open or a specified exception occurs
-    private String app2Fallback() {
         return "App2 is currently unavailable. Please try again later.";
     }
+
 
 
 }
